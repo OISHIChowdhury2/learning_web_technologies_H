af112cc1db7d001ed81430942460b054932594e9
Oishi <Oishichowdhury2@gmail.com>
2021-03-14 13:33:53 +0600

    3nd

---
+++ MID_LAB_EXAM/header.php
@@ -0,0 +1,8 @@
+<?php
+	session_start();
+	if(!isset($_SESSION['flag']))
+	{
+		header('location: singin.html');
+	}
+?>
+
---
+++ MID_LAB_EXAM/profile.php
@@ -0,0 +1,54 @@
+<?php
+	$title= "profile page";
+	include('header.php');
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>profile</title>
+</head>
+<body>
+	<table align="center" border="1" width="20%">
+		<tr>
+			<td colspan="2" align="center">
+				<label>Profile</label>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>ID</label>
+			</td>
+			<td>
+				<label>18-101010-2</label>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>Name</label>
+			</td>
+			<td>
+				<label>Bod</label>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>User Type</label>
+			</td>
+			<td>
+				<label>Admin</label>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2" align="right">
+				<a href="admin.php">Go Home</a>
+			</td>
+		</tr>
+
+
+
+	</table>
+
+</body>
+</html>
